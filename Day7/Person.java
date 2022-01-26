7d2e01bb9dbe01bd41ea11dc22632b8afa4a079a
ShubhamGg2001 <shubham.garg@bigohtech.com>
2022-01-26 14:27:50 +0530

    Messsage

---
+++ Day7/Person.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+
+public class Person {
+    String firstName;
+    String lastNsme;
+    int age;
+    Address address;
+    Map<String, String>phoneNumbers;
+
+}
+
+class Address{
+    String streetAddresss;
+    String city;
+    String state;
+    int postalCode;
+
+}
